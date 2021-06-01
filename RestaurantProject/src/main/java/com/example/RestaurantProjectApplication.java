726d4c4067a4a91cebe238dc6d3fd5f8a5b61498
zubcr7 <59473038+zubcr7@users.noreply.github.com>
2021-06-01 17:17:06 +0530

    Update RestaurantProjectApplication.java

--- RestaurantProject/src/main/java/com/example/RestaurantProjectApplication.java
+++ RestaurantProject/src/main/java/com/example/RestaurantProjectApplication.java
@@ -3,11 +3,37 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+
 @SpringBootApplication
-public class RestaurantProjectApplication {
+public class RestaurantProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantProjectApplication.class, args);
 	}
+	
+	[5:12 PM] Vaddadi Pranathi
+    
+
+
+	@Override
+
+ 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+ 
+	return application.sources(RestaurantProjectApplication.class);
+
+ 
+	}
+
+
+
+
+
 
 }
